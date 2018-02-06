e19a3ada05e8870d8494cb30baac2d63166db7eb
Vishal <vishal114186@gmail.com>
2018-02-06 22:50:38 +0530

    Added datalists

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -11,5 +15,26 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add(0,"One");
+        words.add(1,"Two");
+        words.add(2,"Three");
+        words.add(3,"Four");
+        words.add(4,"Five");
+        words.add(5,"Six");
+        words.add(6,"Seven");
+        words.add(7,"Eight");
+        words.add(8,"Nine");
+        words.add(9,"Ten");
+
+        int count = 0;
+        LinearLayout root = (LinearLayout) findViewById(R.id.rootView);
+
+        while(count != 10){
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(count));
+            root.addView(wordView);
+        }
     }
 }
